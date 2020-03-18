f44dd9753384f10c038bececae08d19d75e1c7f4
Anibal Fernando Antonelli <anibal.anto@gmail.com>
2020-03-18 17:37:57 -0300

    Agregada la Ola a los mundos

--- ejemplo-sistdist/ejemplo-sistdist/src/ejemplo/sistdist/EjemploSistdist.java
+++ ejemplo-sistdist/ejemplo-sistdist/src/ejemplo/sistdist/EjemploSistdist.java
@@ -16,7 +16,7 @@ public class EjemploSistdist {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Ola mundo!");
+        System.out.println("Ola mundos!");
     }
     
 }
